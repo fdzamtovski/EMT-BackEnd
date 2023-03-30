5f1eacce749e0ae6bf64ecc3660dc89a247a7259
fdzamtovski <76556606+fdzamtovski@users.noreply.github.com>
2023-03-30 11:23:22 +0200

    chaning Origins back2

--- EMT-Backend-master/src/main/java/mk/ukim/finki/emt/backend/web/CategoryRestController.java
+++ EMT-Backend-master/src/main/java/mk/ukim/finki/emt/backend/web/CategoryRestController.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/categories")
 public class CategoryRestController {
 
