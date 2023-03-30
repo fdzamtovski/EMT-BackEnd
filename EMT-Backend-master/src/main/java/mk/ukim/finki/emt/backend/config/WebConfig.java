fabede555e80e733bada70c18390cd20faaee7b8
fdzamtovski <76556606+fdzamtovski@users.noreply.github.com>
2023-03-30 11:59:33 +0200

    config commit 3

---
+++ EMT-Backend-master/src/main/java/mk/ukim/finki/emt/backend/config/WebConfig.java
@@ -0,0 +1,14 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
